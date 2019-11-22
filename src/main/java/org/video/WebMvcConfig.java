c8e29e5c0241428175538ca286fc9d30ac9d80c1
coderookier <gupp0816@163.com>
2019-11-22 15:15:23 +0800

    后端请求拦截器

--- src/main/java/org/video/WebMvcConfig.java
+++ src/main/java/org/video/WebMvcConfig.java
@@ -1,8 +1,11 @@
 package org.video;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.video.web.interceptor.MiniInterceptor;
 
 /**
  * @author gutongxue
@@ -15,5 +18,27 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")
                 .addResourceLocations("file:D:/wxxcx/userfiles/");
+    }
+
+    /**
+     * 注册拦截器到spring中
+     * @return
+     */
+    @Bean
+    public MiniInterceptor miniInterceptor() {
+        return new MiniInterceptor();
+    }
+
+    /**
+     * 将自定义的拦截器注册到拦截器中心
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //通过registry进行注册
+        registry.addInterceptor(miniInterceptor()).addPathPatterns("/user/**")
+                .addPathPatterns("/video/upload", "/video/uploadCover")
+                                                  .addPathPatterns("/bgm/**");
+        super.addInterceptors(registry);
     }
 }
---
+++ src/main/java/org/video/web/interceptor/MiniInterceptor.java
@@ -0,0 +1,95 @@
+package org.video.web.interceptor;
+
+import com.mysql.jdbc.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.video.common.utils.IMoocJSONResult;
+import org.video.common.utils.JsonUtils;
+import org.video.common.utils.RedisOperator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author gutongxue
+ * @date 2019/11/22 13:39
+ * 拦截器
+ **/
+public class MiniInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    public RedisOperator redisOperator;
+
+    public static final String USER_REDIS_SESSION = "user-redis-session";
+
+    /**
+     * 拦截请求，在controller调用之前
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        /**
+         * 返回false代表请求被拦截
+         * 返回true表示请求放行
+         */
+        String userId = request.getHeader("userId");
+        //登录成功后生成的唯一token
+        String userToken = request.getHeader("userToken");
+        if (!StringUtils.isEmptyOrWhitespaceOnly(userId) && !StringUtils.isEmptyOrWhitespaceOnly(userToken)) {
+            String uniqueToken = redisOperator.get(USER_REDIS_SESSION + ":" + userId);
+            if (StringUtils.isEmptyOrWhitespaceOnly(uniqueToken)) {
+                System.out.println("会话已过期，请登录");
+                //将信息返回到前端
+                returnErrorResponse(response, IMoocJSONResult.errorTokenMsg("请登录..."));
+                return false;
+            } else {
+                if (!uniqueToken.equals(userToken)) {
+                    System.out.println("账号在别的地方登录");
+                    returnErrorResponse(response, IMoocJSONResult.errorTokenMsg("其他位置登录..."));
+                    return false;
+                }
+            }
+        } else {
+            returnErrorResponse(response, IMoocJSONResult.errorTokenMsg("请登录..."));
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 将拦截信息以json形式返回到前端进行显示
+     */
+    public void returnErrorResponse(HttpServletResponse response, IMoocJSONResult result) throws IOException, UnsupportedEncodingException {
+        OutputStream outputStream = null;
+        try {
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("text/json");
+            outputStream = response.getOutputStream();
+            outputStream.write(JsonUtils.objectToJson(result).getBytes("utf-8"));
+            outputStream.flush();
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+    /**
+     * 拦截请求在controller之后，渲染视图之前
+     */
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 拦截请求在controller之后，渲染视图之后
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
